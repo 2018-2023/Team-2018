87a89244a1d469e3cf09806618b907be8a8a307b
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-06-09 18:43:36 +0900

    ✨ GeolocationAPIの導入、現在地をピン留めされるようにした

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,11 @@
       <!-- 表示するタイル持ってくるURL -->
       <l-tile-layer :url="url" :attribution="attribution" />
       <!-- 以下、ピンやポップアップの指定(取ってきた緯度・経度の個数分ループさせればいいかも) -->
-      <l-marker :lat-lng="withTooltip">
+      <l-marker :lat-lng="center">
         <l-tooltip :options="{ permanent: true, interactive: true }">
           <div @click="handleToolTipClick">
-            I am a tooltip
-            <p v-show="showParagraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
-              Donec finibus semper metus id malesuada.
-            </p>
+            あなたは今ここにいます。<br />you are here.
+            <p v-show="showParagraph">ここにはお店の情報が入ります</p>
           </div>
         </l-tooltip>
       </l-marker>
@@ -45,7 +41,7 @@ export default {
     return {
       // 初期値
       zoom: 13,
-      center: latLng(35.6811, 139.7672),
+      center: [0, 0],
       // 今表示してるタイルのURL
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 
@@ -59,6 +55,16 @@ export default {
     handleToolTipClick() {
       this.showParagraph = !this.showParagraph
     },
+    // 取得した位置情報を処理する
+    success(position) {
+      // console.log(position)
+      // console.log(this.center)
+      this.center = [position.coords.latitude, position.coords.longitude]
+      // console.log(this.center)
+    },
+  },
+  mounted() {
+    navigator.geolocation.getCurrentPosition(this.success)
   },
 }
 
