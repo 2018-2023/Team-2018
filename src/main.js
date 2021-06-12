0017753133fcfebd5ab1d0495083260d3e59e9f0
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-06-12 16:09:34 +0900

    💄 resetCSSの導入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue"
 import router from "./router"
 // 地図ライブラリの導入
 import "leaflet/dist/leaflet.css"
+// resetCSSを適用する
+import "@/global.css"
 
 // FontAwesomeを使うための処理
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
