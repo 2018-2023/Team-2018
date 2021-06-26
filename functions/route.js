92e73f990ebf48fedc7c662d028ec33f415b4471
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-06-26 15:16:20 +0900

    ♻️ functions修正

--- functions/route.js
+++ functions/route.js
@@ -9,16 +9,11 @@ router.route("/:lat/:lng").get(async (req, res) => {
   const API_KEY = "e9cd52a7ae2c9ea3"
   const format = "json"
   const lat = String(Number(req.params.lat) / 10000000)
-  console.log(lat)
   const lng = String(Number(req.params.lng) / 10000000)
-  console.log(lng)
   const range = "5"
   const count = "100"
   const response = await axios.get(
     `${HOTPEPPER_URL}?key=${API_KEY}&lat=${lat}&lng=${lng}&range=${range}&count=${count}&format=${format}`
-  )
-  console.log(
-    `${HOTPEPPER_URL}?key=${API_KEY}&lat=${req.params.lat}&lng=${req.params.lng}&range=${range}&count=${count}&format=${format}`
   )
   const data = response.data
   return res.json(data)
